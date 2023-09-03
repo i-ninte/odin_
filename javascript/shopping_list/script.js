9e2fd830ff971e751943016469f9b7216095832d
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-09-03 19:41:59 +0000

    update script.js

---
+++ javascript/shopping_list/script.js
@@ -0,0 +1,36 @@
+// creating and adding a new element to the list of shopping items
+
+function createItems(item){
+const li= document.createElement('li')
+li.innerHTML= `${item}
+<button class="remove-item btn-link text-red">
+<i class="fa-solid fa-xmark"></i>
+</button>`
+
+	
+	document.querySelector('.items').appendChild(li);
+}
+
+createItems('Egg')
+
+
+
+function createlist(items){
+const li = document.createElement('li');
+li.appendChild(document.createTextNode(item));
+
+const button =document.createElement('button);
+button.className= 'remove-item btn-link text-red';
+	
+
+const icon= document.createElement('i);
+icon.className= 'fa-solid fa-xmark';
+
+	
+	button.appendChild(icon);
+	li.appendChild(button);
+	
+document.querySelector('.items').appendChild(li);
+
+}
+createlist('Cheese');
