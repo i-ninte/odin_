f9dbf65a28a6ff76c404c3041f92daec6496f11c
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-09-04 20:16:02 +0000

    Update app.js

--- javascript/components-projects/02-counter/app.js
+++ javascript/components-projects/02-counter/app.js
@@ -1,1 +1,39 @@
+// Set initial count
+let count = 0;
+
+// Select value and buttons
+const value = document.querySelector("#value");
+
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList; // to get the contents of the class
+    if (styles.contains("decrease")) {
+      count--;
+    } 
+  else if (styles.contains("increase")){
+  count++;
+  }
+	  else {
+	  count=0;
+	  }
+	  
+	  if (count > 0){
+	  value.style.color= 'green';
+	  }
+	   else if (count < 0){
+	  value.style.color= 'red';
+	  }
+	  
+	  else {
+	  value.style.color='#222';
+	  }
+    value.textContent = count;
+	  
+	  
+	  
+	  
+  });
+});
 
