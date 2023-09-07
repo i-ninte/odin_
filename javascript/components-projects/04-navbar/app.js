34e8d4fc7e2fd3c0a32d31b3f7801decc46c8c4a
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-09-07 12:48:40 +0000

    Update app.js

--- javascript/components-projects/04-navbar/app.js
+++ javascript/components-projects/04-navbar/app.js
@@ -4,3 +4,31 @@
 // remove - remove class
 // toggle - toggles class
 
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+/*
+navToggle.addEventListener('click', function () {
+    // console.log(links.classList);
+    if (links.classList.contains('show-links')) {
+        links.classList.remove('show-links');
+    } else {
+        links.classList.add('show-links');
+    }
+});
+
+
+*/
+
+// Using the Toggle Method
+
+navToggle.addEventListener('click', function () {
+    
+   
+        links.classList.toggle('show-links');
+    
+});
+
+
+
+
