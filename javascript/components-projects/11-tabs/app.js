63c1ebfa3569481612d942eb1fca0cfa1e026d40
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-09-13 02:17:52 +0000

    Update app.js

--- javascript/components-projects/11-tabs/app.js
+++ javascript/components-projects/11-tabs/app.js
@@ -1,1 +1,37 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
+const giveaway= document.querySelector('.giveaway');
+const deadline= document.querySelector('.deadline');
+const items= document.querySelectorAll('.deadline-format');
+
+let futureDate= new Date(2023,4,24,15,13,8);
+
+const year= futureDate.getFullYear();
+const hours= futureDate.getHours();
+const minutes= futureDate.getMinutes();
+
+
+
+giveaway.textContent= `giveaway ends on ${year} ${hours}:${minutes} am`;
