a2fd98b5964b2571884af854f33556ad218c6fd1
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-09-23 22:15:00 +0000

    Update app.js

--- javascript/components-projects/12-countdown-timer/app.js
+++ javascript/components-projects/12-countdown-timer/app.js
@@ -24,14 +24,85 @@ const weekdays = [
 
 const giveaway= document.querySelector('.giveaway');
 const deadline= document.querySelector('.deadline');
-const items= document.querySelectorAll('.deadline-format');
+const items= document.querySelectorAll('.deadline-format h4');
 
-let futureDate= new Date(2023,4,24,15,13,8);
+let futureDate= new Date(2023,10,24,15,13,8);
 
 const year= futureDate.getFullYear();
 const hours= futureDate.getHours();
 const minutes= futureDate.getMinutes();
 
+let month = futureDate.getMonth();
+month= months[month];
+const date = futureDate.getDate();
+let weekday = futureDate.getDay();
+
+weekday= weekdays[weekday];
+
+giveaway.textContent= `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes} am`;
+
+// getting the time in miliseconds 
+
+const futureTime= futureDate.getTime();
 
 
-giveaway.textContent= `giveaway ends on ${year} ${hours}:${minutes} am`;
+function getRemainingTime(){
+	 const today= new Date().getTime();
+	 const t = futureTime - today;
+	 
+	 const oneDay = 86400000;
+	 const oneHour= 3600000;
+	const oneMinute= 60000;
+	const oneSec= 1000;
+	
+	
+	// calculate days
+let	days= t/oneDay;
+	days = Math.floor(days);
+	
+	let hours= (t % oneDay) / oneHour;
+	 hours = Math.floor(hours);
+	
+	
+	let min = (t % oneHour) / oneMinute;
+	min= Math.floor(min);
+	
+	let sec = (t % oneMinute)/ oneSec;
+   sec=Math.floor(sec);
+	
+	
+	// conversions
+	/* 1 sec = 1000ms
+	1 minute = 60000
+	1 hour = 3600 sec // 3600000 ms
+	1 day = 24 * 3600000 ms = 86400000 ms
+	*/
+	
+	
+	// set values 
+		const values = [days, hours, min, sec ];
+	
+	// Assuming you have defined the items and values arrays somewhere in your code
+
+function format(item) {
+    if (item < 10) {
+        return `0${item}`;
+    } else {
+        return item;
+    }
+}
+
+items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+});
+	if (t< 0){
+	clearInterval(countdown);	
+	deadline.innerHTML = `<h4 class ="expired"> Sorry this giveaway has expired </h4>`
+	}
+
+}
+
+// countdown
+// setInterval repeatedly runs the function for every time you pass in the second argument
+let countdown = setInterval(getRemainingTime, 1000);
+getRemainingTime();
